f9f041d68b1613dd1076721699f955d06b98f927
JetsadaWijit <jetsadawijit@outlook.com>
2024-12-14 05:02:17 +0000

    set up

---
+++ mcengine-vault-api/src/main/java/io/github/mcengine/api/MCEngineVaultApi.java
@@ -0,0 +1,3 @@
+package io.github.mcengine.api;
+
+public class MCEngineVaultApi {}
---
+++ spigotmc/src/main/java/io/github/mcengine/MCEngineVault.java
@@ -0,0 +1,12 @@
+package io.github.mcengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCEngineVault extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
