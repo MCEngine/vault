538926ea3a74966f1ae603479765589e7e58d966
JetsadaWijit <jetsadawijit@outlook.com>
2024-12-27 09:00:03 +0000

    Create MCEngineVaultApiItem.java

---
+++ mcengine-vault-api/src/main/java/io/github/mcengine/api/vault/item/MCEngineVaultApiItem.java
@@ -0,0 +1,9 @@
+package io.github.mcengine.api.vault.item;
+
+class MCEngineVaultApiItem {
+    private final String password;
+
+    public MCEngineVaultApiItem(Plugin plugin, String password) {
+        this.password = password;
+    }
+}
