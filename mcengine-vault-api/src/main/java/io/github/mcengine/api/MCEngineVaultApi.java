1aa685b28938071dc99bba366072d309b7ae7046
JetsadaWijit <jetsadawijit@outlook.com>
2024-12-27 08:58:02 +0000

    Clear

--- mcengine-vault-api/src/main/java/io/github/mcengine/api/MCEngineVaultApi.java
+++ mcengine-vault-api/src/main/java/io/github/mcengine/api/MCEngineVaultApi.java
@@ -1,3 +1,15 @@
 package io.github.mcengine.api;
 
-public class MCEngineVaultApi {}
+public class MCEngineVaultApi {
+    private final String password;
+
+    public MCEngineVaultApi(Plugin plugin, String password) {
+        this.password = password;
+    }
+
+    public void createVault(String password, int size) {}
+
+    public String getPassword() {
+        return password;
+    }
+}
